aead4925d68df953d1905048ae80703cffc86f64
YeSv <yegor.sergeev@gmail.com>
2021-09-05 14:41:21 +0300

    Remove healthchecker, zero partitions code

--- src/Simple.Kafka.Rpc/Config.cs
+++ src/Simple.Kafka.Rpc/Config.cs
@@ -6,7 +6,6 @@ namespace Simple.Kafka.Rpc
     {
         public string[] Topics { get; set; } = Array.Empty<string>();
         public bool StopConsumerOnUnhandledException { get; set; }
-        public bool RecreateConsumerOnZeroPartitionsAssigned { get; set; }
         public TimeSpan? RequestTimeout { get; set; }
         public TimeSpan ConsumerHealthCheckInterval { get; set; } = TimeSpan.FromSeconds(10);
         public TimeSpan ProducerHealthCheckInterval { get; set; } = TimeSpan.FromSeconds(10);
